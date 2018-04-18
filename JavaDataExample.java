365df143afa8d7c1e1d88fa284dbe8c4a54aeeec
sri28vara <sri28vara@gmail.com>
2018-04-18 05:29:40 +0000

    data java code

---
+++ JavaDataExample.java
@@ -0,0 +1,13 @@
+public class JavaDateExample{
+ 
+  public static void main(String args[]){
+  /*
+  Create date object with current date and time.
+  */
+  
+  Date date = new Date();
+  System.out.println("Today is " + date);
+  
+  }
+ 
+}
