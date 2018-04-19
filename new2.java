40a7f8be0dc43c4ed51c3ba8683128b4f1958bf6
sri28vara <sri28vara@gmail.com>
2018-04-19 09:57:42 +0000

    new 2 java code and new 3 html code

---
+++ new2.java
@@ -0,0 +1,7 @@
+c class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+      System.out.println(" Exploring Java world in new 2 ");
+   }
+}
