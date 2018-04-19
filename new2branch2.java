1c18d07b5256e67852a8e587e0a651db2d8008d4
sri28vara <sri28vara@gmail.com>
2018-04-19 10:25:03 +0000

    new branch2 code added

---
+++ new2branch2.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+System.out.println(" new 2 beanch 2 java code ");
+System.out.println(" Java is Munlti threaded ");
+
+}
+}
+
+
+
