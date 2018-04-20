93822fed84d63fd3909c702f80d5e02e7142e5c4
sri28vara <sri28vara@gmail.com>
2018-04-20 09:39:34 +0000

    java stash code is finished

---
+++ Javastash.java
@@ -0,0 +1,28 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+         
+ }
+    public int verifyCreditCard(arg1 ,arg2 ) 
+{
+
+   // verifying the credit card data from the data base
+}
+   public  hashmap fetchUserData( arg 1) 
+{
+   // fetching the user data 
+   
+}
+
+   public boolean authenticateUserData()
+{
+
+}
+   public boolean connectRemote ()
+ {
+ }
+      System.out.println( " finished writing the code ");
+}
+
