97586c48edbad125145b2e3ad65fc2f0ff7c204d
sri28vara <sri28vara@gmail.com>
2018-04-22 13:48:04 +0000

    java pull revise code

---
+++ javapullrevise.java
@@ -0,0 +1,10 @@
+public class Demo
+{
+  public static void main(String args[])
+  {
+    System.out.println("Good Morning");
+    System.out.println("Hello World");
+    Ssytem.out.println( " updated in local repo ");      
+
+}
+}
