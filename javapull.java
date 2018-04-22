38d31ead2769b14e93263c64216ab9fede7de682
sri28vara <38359706+sri28vara@users.noreply.github.com>
2018-04-22 18:51:22 +0530

    Update javapull.java

--- javapull.java
+++ javapull.java
@@ -5,6 +5,8 @@ public class Demo
     System.out.println("Good Morning");
     System.out.println("Hello World");
     System.out.println(" git pull demo demystified ");
-    System.out.println(" unleashing the power of git" );  
+    System.out.println(" unleashing the power of git" ); 
+    System.out.println( " pulling the new java code from remore repo to local repo");
+    System.out.println( " refresh the local repo to see changes ");
 }
 }
