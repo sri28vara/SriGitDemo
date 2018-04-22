c2bcbd4e60ef380fe1e423ad6f057f70cdb181f9
sri28vara <sri28vara@gmail.com>
2018-04-22 13:18:49 +0000

    Java pull code added again

---
+++ javapull.java
@@ -0,0 +1,10 @@
+public class Demo
+{
+  public static void main(String args[])
+  {
+    System.out.println("Good Morning");
+    System.out.println("Hello World");
+    System.out.println(" git pull demo demystified ");
+    System.out.println(" unleashing the power of git" );  
+}
+}
